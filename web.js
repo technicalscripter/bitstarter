3520d23762ddfc3204c5e71eefe49d18d9de38fa
Anay Chowdhary <chowdhary.anay@gmail.com>
2013-09-02 13:28:30 +0000

    Updating web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+require('fs');
+
 app.use(express.logger());
 
+var buf = fs.readFileSync('index.html');
+var final_string = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(final_string);
 });
 
 var port = process.env.PORT || 5000;
